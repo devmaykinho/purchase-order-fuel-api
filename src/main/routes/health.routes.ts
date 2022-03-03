7935f27b0dac53d7805fe76d8346217146f331d5
Maycon Albuquerque <CIANDT\mayconrt@lnb022018cps.ciandt.global>
2022-03-03 18:22:28 -0300

    chore: removing test video

--- src/main/routes/health.routes.ts
+++ src/main/routes/health.routes.ts
@@ -1,12 +1,7 @@
 import { Router, Request, Response } from 'express'
-import axios from 'axios'
 
 export default (router: Router): void => {
   router.get('/health', (req: Request, res: Response) => {
-    axios.get('https://player.vimeo.com').then(() => {
-      res.status(200).json({ description: 'servidor is running' })
-    }).catch((error) => {
-      res.status(200).json({ description: error })
-    })
+    res.status(200).json({ description: 'servidor is running' })
   })
 }
