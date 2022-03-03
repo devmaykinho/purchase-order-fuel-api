ea3358e67bb03f1de4c1f468ac26932d5759c5a8
Maycon Albuquerque <CIANDT\mayconrt@lnb022018cps.ciandt.global>
2022-03-03 15:09:16 -0300

    chore: testing api

--- src/main/routes/health.routes.ts
+++ src/main/routes/health.routes.ts
@@ -1,7 +1,12 @@
 import { Router, Request, Response } from 'express'
+import axios from 'axios'
 
 export default (router: Router): void => {
   router.get('/health', (req: Request, res: Response) => {
-    res.status(200).json({ description: 'servidor is running' })
+    axios.get('https://player.vimeo.com').then(() => {
+      res.status(200).json({ description: 'servidor is running' })
+    }).catch((error) => {
+      res.status(200).json({ description: error })
+    })
   })
 }
